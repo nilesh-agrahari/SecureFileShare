8ce361c867fe212f78b4203ce73a7def4802f0c3
Nilesh Agrahari <nagrahari539@gmail.com>
2025-07-02 12:53:19 +0530

    initial commit

--- securefileshare/securefileshare/settings.py
+++ securefileshare/securefileshare/settings.py
@@ -145,8 +145,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'nxremixstation@gmail.com'
-EMAIL_HOST_PASSWORD = 'tsagtpmuettqwpgk'
+EMAIL_HOST_USER = 'your email id'
+EMAIL_HOST_PASSWORD = 'your password'
 
 
 # Default primary key field type
